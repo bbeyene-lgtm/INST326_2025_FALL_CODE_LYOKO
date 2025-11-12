b3865a63a592d0c44238145b3c144d67d2c2a202
Aggrey Narh <anarh1@terpmail.umd.edu>
2025-11-12 13:25:00 -0500

    Aggrey Work

--- collab_excersice.py
+++ collab_excersice.py
@@ -2,3 +2,11 @@ print("One Piece is the best anime ever!") #Aggrey
 print("Aric is a jerk (Joking, spare me)") #Lizandro 
 print("I love cats") #Johana
 print("lets skip this exam!!") #Beimnet
+
+
+import sys
+import argparse
+
+
+def countingLetters(
+   
