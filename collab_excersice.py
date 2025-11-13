a80b5daa521dd0108019261024945cf52fcfeb12
Johana Hernandez <jherna35@terpmail.umd.edu>
2025-11-13 14:46:02 -0500

    started working on the algorithm

--- collab_excersice.py
+++ collab_excersice.py
@@ -10,4 +10,13 @@ import argparse
 
 def countingLetters():
     pass
-   
+
+#Input validation and string matching 
+def input_validation (guess, word_list, guessed_words):
+    if not isinstance(guess, str) or not guess.strip():
+        return False, "Invalid input. Please enter a word,"
+    
+    normalized_guess = guess.strip().lower 
+    
+    if normalized_guess in guessed_words: 
+        return False, f"You already guessed' '{normalized_guess}'. Try a new word."
