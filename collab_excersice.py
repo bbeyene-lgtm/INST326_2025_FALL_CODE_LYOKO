1f8d8206d104a974522ed050baaf76620ab6e67f
Johana Hernandez <jherna35@terpmail.umd.edu>
2025-11-13 16:24:24 -0500

    finished my part of the algorithm

--- collab_excersice.py
+++ collab_excersice.py
@@ -13,10 +13,32 @@ def countingLetters():
 
 #Input validation and string matching 
 def input_validation (guess, word_list, guessed_words):
+    
+    """
+    Validates a players guess in a word hunt game 
+    
+    Parameters:
+        guess(str): The word guessed by the user 
+        word_list(set): The set of valid words for the current topic ot round.
+        guessed_words(set): The set of words the player has already guessed 
+        
+    Returns: 
+        tuple: (is_valid, message)[].
+        is_valid (bool): True is the guess is valid and new, False otherwise. 
+        message(str): Explanation of the result
+    """
+    
     if not isinstance(guess, str) or not guess.strip():
         return False, "Invalid input. Please enter a word,"
     
     normalized_guess = guess.strip().lower 
     
     if normalized_guess in guessed_words: 
-        return False, f"You already guessed' '{normalized_guess}'. Try a new word."
+        return False, f"You already guessed '{normalized_guess}'.Try another new word."
+    
+    if normalized_guess not in word_list: 
+        return False, f"'{normalized_guess}' is not a valid word for this round."
+    
+    guessed_words.add(normalized_guess)
+
+    return True, f"'{normalized_guess}' is a valid word."
