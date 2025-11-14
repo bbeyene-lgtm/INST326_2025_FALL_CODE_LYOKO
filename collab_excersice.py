a508e1adb31540ed972de7cca3bcd8fa0b4764ad
Aggrey Narh <anarh1@terpmail.umd.edu>
2025-11-14 00:57:56 -0500

    Hi there

--- collab_excersice.py
+++ collab_excersice.py
@@ -8,6 +8,18 @@ import sys
 import argparse
 
 
-def countingLetters():
-    pass
+#example topic not real
+plants = ["trees", "flowers", "shrubs", "herbs","tulips","roses","daisies","bushes"
+          "ferns","mosses","cacti","vines","grasses","palms","sunflowers","lilies",
+          "orchids","daffodils","irises","marigolds","lavender"]
+
+alphabetlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def LCD():
+    print("Welcome to the word game!")
+    gamepick = input("Please pick a topic from the following list:\n"
+                     "1.plants\n"
+                     "2. animals\n")
+    guess = input("Using the space given enter your plant guesses: ")
+    
    
