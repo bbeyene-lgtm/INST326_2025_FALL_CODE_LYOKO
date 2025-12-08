9b70310f4647c701271f5c6b7feafb8ecd37d4c2
Johana Hernandez <jherna35@terpmail.umd.edu>
2025-12-08 09:50:05 -0500

    code

--- collab_excersice.py
+++ collab_excersice.py
@@ -54,7 +54,7 @@ def input_validation (guess, word_list, guessed_words):
     
     """
     Validates a players guess in a word hunt game 
-gt
+
     Parameters:
         guess(str): The word guessed by the user 
         word_list(set): The set of valid words for the current topic ot round.
@@ -145,7 +145,6 @@ class Grid:
             for letter in row:
                 print(letter, end=" ")
             print()  
-
 
 
 if __name__ == "__main__":
